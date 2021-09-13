9b672a516f5bc61cebed8b79202895ec33568986
ArdeleanRazvan <ardelean.razvan.c@gmail.com>
2021-09-13 13:27:04 +0300

    Added Saved Variables for NameplatesSCT

---
+++ NameplateSCT.lua
@@ -0,0 +1,33 @@
+
+NameplateSCTDB = {
+	["profileKeys"] = {
+		["Petmaistar - Blackrock [PvP only]"] = "Default",
+		["Razkuff - ChromieCraft"] = "Default",
+		["Nethannos - Blackrock [PvP only]"] = "Default",
+		["Nethanos - Icecrown"] = "Default",
+		["Razkkain - Blackrock [PvP only]"] = "Default",
+		["Razkuff - Blackrock [PvP only]"] = "Default",
+		["Kadronv - Blackrock [PvP only]"] = "Default",
+		["Wartok - Blackrock [PvP only]"] = "Default",
+		["Nethanos - ChromieCraft"] = "Default",
+		["Razkain - ChromieCraft"] = "Default",
+		["Erbalistic - Blackrock [PvP only]"] = "Default",
+	},
+	["global"] = {
+		["personal"] = true,
+		["yOffset"] = -50,
+		["showIcon"] = false,
+		["damageColor"] = false,
+		["font"] = "Gotham Narrow Ultra",
+		["sizing"] = {
+			["critsScale"] = 1.25,
+		},
+		["animations"] = {
+			["autoattackcrit"] = "fountain",
+			["miss"] = "fountain",
+			["animationspeed"] = 2,
+			["crit"] = "fountain",
+		},
+		["textShadow"] = true,
+	},
+}
